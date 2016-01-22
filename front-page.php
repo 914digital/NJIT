7da23ac1168f17e7d9b712cdb8cfdfda3ded895a
danhanee <dan@haneedesigns.com>
2016-01-22 16:32:48 -0500

    Initial Commit

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Home Page
+*/
+?>
+
+<?php get_template_part('parts/header'); ?>
+
+<div class="container">
+  <div class="row">
+
+    <div class="col-md-12">
+     </div><!-- /.col-md-12 -->
+    
+  </div><!-- /.row -->
+</div><!-- /.container -->
+
+<?php get_template_part('parts/footer'); ?>
---
+++ parts/footer.php
@@ -0,0 +1,16 @@
+<div class="container footer">
+ 
+	<div class="row">
+    <?php dynamic_sidebar('footer-widget-area'); ?>
+  </div>
+ 
+  <div class="row">
+    <div class="col-lg-12">
+      <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+    </div>
+  </div>
+</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
